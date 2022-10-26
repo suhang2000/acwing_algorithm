b117e0cc8eaa3e160f3f8801c0cdacf9962495ec
LI Suhang <lisuhang2000@gmail.com>
2022-10-26 15:10:50 +0900

    chapter1: high precision -> subtraction

--- Basic/Chapter1/HighPrecision.py
+++ Basic/Chapter1/HighPrecision.py
@@ -32,3 +32,63 @@ if carry:
 
 for x in res:
     print(x, end='')
+
+"""
+792. 高精度减法
+https://www.acwing.com/problem/content/794/
+
+输入样例：
+32
+11
+输出样例：
+21
+"""
+
+a = [int(x) for x in list(input())]
+b = [int(x) for x in list(input())]
+
+
+# whether a >= b
+def cmp(a, b):
+    if len(a) != len(b): return len(a) > len(b)
+    for x, y in zip(a, b):
+        if x < y:
+            return False
+        elif x > y:
+            return True
+    return True
+
+
+# 1. 判断大小，正负
+minus = False
+if not cmp(a, b):
+    # a < b
+    a, b = b, a
+    minus = True
+
+a.reverse()
+b.reverse()
+
+# 2. 逐位相减
+res = collections.deque()
+carry = 0
+n, m = len(a), len(b)
+for i in range(n):
+    cur = a[i] - carry
+    if i < m:
+        cur -= b[i]
+    if cur < 0:
+        cur += 10
+        carry = 1
+    else:
+        carry = 0
+    res.appendleft(cur)
+
+# 3. 移出前导0
+while len(res) > 1 and res[0] == 0:
+    res.popleft()
+
+if minus:
+    print("-", end='')
+for x in res:
+    print(x, end='')
