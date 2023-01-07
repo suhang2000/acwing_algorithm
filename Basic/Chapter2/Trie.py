fe8b5e50d0c2fc7b0f2e52d4fba7f17deec1a41d
LI Suhang <lisuhang2000@gmail.com>
2023-01-08 00:41:13 +0900

    chapter2: 01Trie

--- Basic/Chapter2/Trie.py
+++ Basic/Chapter2/Trie.py
@@ -77,3 +77,83 @@ for _ in range(n):
     else:
         print(search(x))
 
+"""
+143. 最大异或对
+https://www.acwing.com/problem/content/145/
+"""
+
+
+class Trie01:
+    def __init__(self):
+        self.children = collections.defaultdict(Trie01)
+
+    def insert(self, x):
+        node = self
+        for i in range(30, -1, -1):
+            node = node.children[x >> i & 1]
+
+    def query(self, x):
+        node = self
+        res = 0
+        for i in range(30, -1, -1):
+            p = x >> i & 1
+            if p ^ 1 in node.children:
+                res += 1 << i
+                node = node.children[p ^ 1]
+            elif p in node.children:
+                node = node.children[p]
+            else:
+                break
+        return res
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+trie = Trie01()
+
+for x in nums:
+    trie.insert(x)
+
+print(max(trie.query(x) for x in nums))
+
+# 手动构造数组模拟
+
+N = 3100010
+son = [[0, 0] for _ in range(N)]
+idx = 0
+
+
+def insert(x):
+    global idx
+    p = 0
+    for i in range(30, -1, -1):
+        cur = x >> i & 1
+        if son[p][cur] == 0:
+            idx += 1
+            son[p][cur] = idx
+        p = son[p][cur]
+
+
+def search(x):
+    p = res = 0
+    for i in range(30, -1, -1):
+        cur = x >> i & 1
+        if son[p][cur ^ 1]:
+            res += 1 << i
+            p = son[p][cur ^ 1]
+        # else:
+        #     p = son[p][cur]
+        elif son[p][cur]:
+            p = son[p][cur]
+        else:
+            break
+    return res
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+res = 0
+for x in nums:
+    insert(x)
+    res = max(res, search(x))
+print(res)
