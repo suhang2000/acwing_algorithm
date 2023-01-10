9362b910b1af26dbcb0eae88928682573ae0a06e
LI Suhang <lisuhang2000@gmail.com>
2023-01-10 23:12:46 +0900

    chapter2: Union Find with size

--- Basic/Chapter2/UnionFind.py
+++ Basic/Chapter2/UnionFind.py
@@ -16,6 +16,27 @@ class UnionFind:
         self.p[self.find(x)] = self.find(y)
 
 
+class UnionFind2:
+    def __init__(self, n):
+        self.p = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def merge(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return
+        self.p[px] = py
+        self.size[py] += self.size[px]
+
+    def get_size(self, x):
+        return self.size[self.find(x)]
+
+
 """
 836. 合并集合
 https://www.acwing.com/problem/content/838/
@@ -30,3 +51,19 @@ for _ in range(m):
         uf.merge(a, b)
     else:
         print("Yes" if uf.find(a) == uf.find(b) else "No")
+
+"""
+837. 连通块中点的数量
+https://www.acwing.com/problem/content/839/
+"""
+
+n, m = map(int, input().split())
+uf = UnionFind2(n + 1)
+for _ in range(m):
+    line = input().split()
+    if line[0] == 'C':
+        uf.merge(int(line[1]), int(line[2]))
+    elif line[0] == 'Q1':
+        print("Yes" if uf.find(int(line[1])) == uf.find(int(line[2])) else "No")
+    else:
+        print(uf.get_size(int(line[1])))
