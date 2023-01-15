66eb9a34aa72959a5aa3869cd1eca8e0e167b466
LI Suhang <lisuhang2000@gmail.com>
2023-01-16 03:08:58 +0900

    chapter2: Hash Closed Hashing

--- Basic/Chapter2/Hash.py
+++ Basic/Chapter2/Hash.py
@@ -5,7 +5,7 @@ Hash
 840. 模拟散列表
 https://www.acwing.com/problem/content/842/
 """
-# 拉链法
+# 拉链法 open hashing
 N = 100003
 h, e, ne = [-1] * N, [0] * N, [0] * N
 idx = 0
@@ -38,3 +38,28 @@ for _ in range(n):
         insert(x)
     else:
         print("Yes" if query(x) else "No")
+
+# 开放寻址法 closed hashing
+N = 200003
+null = 0x3f3f3f3f
+h = [null] * N
+
+
+def find(x):
+    k = x % N
+    while h[k] != null and h[k] != x:
+        k += 1
+        if k == N:
+            k = 0
+    return k
+
+
+n = int(input())
+for _ in range(n):
+    op, x = input().split()
+    x = int(x)
+    k = find(x)
+    if op == 'I':
+        h[k] = x
+    else:
+        print("Yes" if h[k] != null else "No")
