84ec9cb2c4ddf71f6fe6a63a65003dc8c38c4b24
LI Suhang <lisuhang2000@gmail.com>
2023-01-20 21:51:56 +0900

    chapter3: BFS 八数码

--- Basic/Chapter3/BFS.py
+++ Basic/Chapter3/BFS.py
@@ -32,3 +32,40 @@ def bfs():
 
 
 print(bfs())
+
+"""
+845. 八数码
+https://www.acwing.com/problem/content/847/
+"""
+
+s = input().split()
+
+
+def dfs(start):
+    end = "12345678x"
+    q = collections.deque()
+    q.append(start)
+    distance = dict()
+    distance[''.join(start)] = 0
+    dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
+    while q:
+        cur = q.popleft()
+        s_cur = ''.join(cur)
+        if s_cur == end:
+            return distance[s_cur]
+        d = distance[s_cur]
+        pre = s_cur.find('x')
+        pre_x, pre_y = pre // 3, pre % 3
+        for i, j in zip(dx, dy):
+            x, y = pre_x + i, pre_y + j
+            if 0 <= x < 3 and 0 <= y < 3:
+                cur[pre], cur[x * 3 + y] = cur[x * 3 + y], cur[pre]
+                s_next = ''.join(cur)
+                if s_next not in distance:
+                    q.append(cur.copy())
+                    distance[s_next] = d + 1
+                cur[pre], cur[x * 3 + y] = cur[x * 3 + y], cur[pre]
+    return -1
+
+
+print(dfs(s))
