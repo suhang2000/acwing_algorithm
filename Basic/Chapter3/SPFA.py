1e2b4df56df54c87eaf3f3e5cf21e512b21e1b32
LI Suhang <lisuhang2000@gmail.com>
2023-01-27 11:31:10 +0900

    chapter3: SPFA: negative cycles

--- Basic/Chapter3/SPFA.py
+++ Basic/Chapter3/SPFA.py
@@ -8,6 +8,7 @@ from math import inf
 """
 851. spfa求最短路
 https://www.acwing.com/problem/content/853/
+用队列存储变短了的点，用这些点去更新后续点
 """
 
 n, m = map(int, input().split())
@@ -43,3 +44,43 @@ def spfa_shortest_path():
 
 res = spfa_shortest_path()
 print(res if res != inf else "impossible")
+
+"""
+852. spfa判断负环
+https://www.acwing.com/problem/content/854/
+把所有点放入队列，判断循环过程中是否有点的路径大于n-1(一共n个点，路径最长不超过n-1)
+"""
+n, m = map(int, input().split())
+g = [[] for _ in range(n + 1)]
+dist = [0] * (n + 1)  # 不需要初始化为inf，不是用来保存最短路径的，只需要记录距离更新信息即可
+state = [True] * (n + 1)
+cnt = [0] * (n + 1)
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    g[x].append((y, z))
+
+
+def spfa_negative_cycles():
+    global n
+    # add all points
+    dq = collections.deque(range(1, n + 1))
+    # for i in range(1, n + 1):
+    #     dq.append(i)
+
+    while dq:
+        x = dq.popleft()
+        state[x] = False
+        for y, z in g[x]:
+            if dist[y] > dist[x] + z:
+                dist[y] = dist[x] + z
+                cnt[y] = cnt[x] + 1
+                if cnt[y] > n - 1:
+                    return True
+                if not state[y]:
+                    dq.append(y)
+                    state[y] = True
+
+    return False
+
+
+print("Yes" if spfa_negative_cycles() else "No")
