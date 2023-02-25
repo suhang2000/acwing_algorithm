4a97c332d6cffe4e2dc10a69f15d25fb4ac4195b
LI Suhang <lisuhang2000@gmail.com>
2023-02-25 18:30:38 +0900

    chapter4: Game Theory: Nim Game (mex)

--- Basic/Chapter4/GameTheory.py
+++ Basic/Chapter4/GameTheory.py
@@ -24,3 +24,31 @@ nums = list(map(int, input().split()))
 # 只计算奇数台阶异或值
 res = reduce(xor, [x for i, x in enumerate(nums) if i % 2 == 0])
 print("Yes" if res else "No")
+
+"""
+893. 集合-Nim游戏
+https://www.acwing.com/problem/content/895/
+"""
+
+
+def sg(x):
+    if cache[x] != -1:
+        return cache[x]
+    state = set()
+    for i in s:
+        if x >= i:
+            state.add(sg(x - i))
+    i = 0
+    while i in state:
+        i += 1
+    cache[x] = i
+    return i
+
+
+k = int(input())
+s = list(map(int, input().split()))
+n = int(input())
+h = list(map(int, input().split()))
+cache = [-1] * 10010
+res = reduce(xor, [sg(x) for x in h])
+print("Yes" if res else "No")
