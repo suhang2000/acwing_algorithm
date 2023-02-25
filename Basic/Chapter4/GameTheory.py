969a29e7a0d57b50425e582a1f586abbac3e6006
LI Suhang <lisuhang2000@gmail.com>
2023-02-25 17:35:33 +0900

    chapter4: Game Theory: Nim Game (odd)

--- Basic/Chapter4/GameTheory.py
+++ Basic/Chapter4/GameTheory.py
@@ -13,3 +13,14 @@ n = int(input())
 nums = list(map(int, input().split()))
 res = reduce(xor, nums)
 print("Yes" if res else "No")
+
+"""
+892. 台阶-Nim游戏
+https://www.acwing.com/problem/content/894/
+"""
+
+n = int(input())
+nums = list(map(int, input().split()))
+# 只计算奇数台阶异或值
+res = reduce(xor, [x for i, x in enumerate(nums) if i % 2 == 0])
+print("Yes" if res else "No")
