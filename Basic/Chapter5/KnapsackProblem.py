3e2c2e6105966c6227dc82b6782f7dc22f7498a7
LI Suhang <lisuhang2000@gmail.com>
2023-03-01 21:20:53 +0900

    chapter5: Knapsack Problem multiple binary optimize

--- Basic/Chapter5/KnapsackProblem.py
+++ Basic/Chapter5/KnapsackProblem.py
@@ -55,3 +55,32 @@ for i in range(n):
                 break
             dp[j] = max(dp[j], dp[j - k * v] + k * w)
 print(dp[-1])
+
+"""
+5. 多重背包问题 II
+https://www.acwing.com/problem/content/5/
+二进制优化，转化为01背包
+"""
+
+n, volume = map(int, input().split())
+v, w = [], []
+for i in range(n):
+    a, b, s = map(int, input().split())
+    k = 1
+    # 二进制优化
+    while k <= s:
+        v.append(k * a)
+        w.append(k * b)
+        s -= k
+        k *= 2
+    if s:
+        # 注意这里是s乘，不是k
+        v.append(s * a)
+        w.append(s * b)
+# 01 knapsack
+m = len(v)
+dp = [0] * (volume + 1)
+for i in range(m):
+    for j in range(volume, v[i] - 1, -1):
+        dp[j] = max(dp[j], dp[j - v[i]] + w[i])
+print(dp[-1])
