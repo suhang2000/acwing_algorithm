2de496e2861ccbe0f23ee1401297787c20871bf0
LI Suhang <lisuhang2000@gmail.com>
2023-03-04 21:01:05 +0900

    chapter5: Linear DP 最长上升子序列

--- Basic/Chapter5/LinearDP.py
+++ Basic/Chapter5/LinearDP.py
@@ -3,6 +3,8 @@ Linear DP
 """
 from math import inf
 
+import cachetools
+
 """
 898. 数字三角形
 https://www.acwing.com/problem/content/900/
@@ -19,3 +21,17 @@ for i in range(1, n):
     for j in range(1, i + 1):
         dp[i][j] = a[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1])
 print(max(dp[-1]))
+
+"""
+895. 最长上升子序列
+https://www.acwing.com/problem/content/897/
+"""
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [1] * n
+for j in range(1, n):
+    for i in range(j):
+        if nums[i] < nums[j]:
+            dp[j] = max(dp[j], dp[i] + 1)
+print(max(dp))
