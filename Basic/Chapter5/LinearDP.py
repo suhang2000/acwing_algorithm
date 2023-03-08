a97be77732d5c971349e6c957b7ddedfaee59362
LI Suhang <lisuhang2000@gmail.com>
2023-03-08 17:18:27 +0900

    chapter5: Linear DP 最短编辑距离

--- Basic/Chapter5/LinearDP.py
+++ Basic/Chapter5/LinearDP.py
@@ -72,3 +72,25 @@ for i in range(1, n + 1):
     for j in range(1, m + 1):
         f[i][j] = f[i - 1][j - 1] + 1 if a[i - 1] == b[j - 1] else max(f[i - 1][j], f[i][j - 1])
 print(f[-1][-1])
+
+"""
+902. 最短编辑距离
+https://www.acwing.com/problem/content/description/904/
+"""
+
+n = int(input())
+a = input()
+m = int(input())
+b = input()
+
+f = [[0] * (m + 1) for _ in range(n + 1)]
+f[0] = list(range(m + 1))
+
+for i in range(1, n + 1):
+    f[i][0] = i
+    for j in range(1, m + 1):
+        if a[i - 1] == b[j - 1]:
+            f[i][j] = f[i - 1][j - 1]
+        else:
+            f[i][j] = min(f[i - 1][j], f[i][j - 1], f[i - 1][j - 1]) + 1
+print(f[n][m])
